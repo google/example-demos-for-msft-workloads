9d3243506745d4585f5c568164400aab790c808b
bkudzia <bkudzia@gmail.com>
2023-12-04 17:01:44 -0600

    initial sync -- code requires testing bofore merging to main

--- windows-cold-dr-async-pd/dr/dr-east-vms.tf
+++ windows-cold-dr-async-pd/dr/dr-east-vms.tf
@@ -14,32 +14,6 @@
  * limitations under the License.
  */
 
-resource "google_compute_instance" "dr-east-dc" {
-  name         = var.failback-east-dc-gce-display-name
-  machine_type = var.east-dc-machine-type
-  zone         = var.dr-dc-zone
-  project      = var.app-dr-east-project
-
-  #  tags = ["foo", "bar"]
-
-  boot_disk {
-    source = "projects/${var.app-dr-east-project}/zones/${var.dr-dc-zone}/disks/${var.failback-east-dc-gce-display-name}-secboot"
-  }
-
-  network_interface {
-    subnetwork = var.east-dr-subnet
-    network_ip = var.east-dr-dc-ip
-  }
-
-  service_account {
-    email  = var.east-dr-sa
-    scopes = var.east-dr-sa-scopes
-  }
-
-  allow_stopping_for_update = true
-
-}
-
 resource "google_compute_instance" "dr-east-vms" {
   depends_on   = [google_compute_instance.dr-east-dc]
   for_each     = local.app_east_dr_vms
@@ -57,6 +31,32 @@ resource "google_compute_instance" "dr-east-vms" {
   network_interface {
     subnetwork = var.east-dr-subnet
     network_ip = data.terraform_remote_state.east_app_server_ip.outputs.east_app_server_ip[each.value.vm_order]
+  }
+
+  service_account {
+    email  = var.east-dr-sa
+    scopes = var.east-dr-sa-scopes
+  }
+
+  allow_stopping_for_update = true
+
+}
+
+resource "google_compute_instance" "dr-east-dc" {
+  name         = var.failback-east-dc-gce-display-name
+  machine_type = var.east-dc-machine-type
+  zone         = var.dr-dc-zone
+  project      = var.app-dr-east-project
+
+  #  tags = ["foo", "bar"]
+
+  boot_disk {
+    source = "projects/${var.app-dr-east-project}/zones/${var.dr-dc-zone}/disks/${var.failback-east-dc-gce-display-name}-secboot"
+  }
+
+  network_interface {
+    subnetwork = var.east-dr-subnet
+    network_ip = var.east-dr-dc-ip
   }
 
   service_account {
